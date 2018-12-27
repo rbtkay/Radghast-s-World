db32f5dcb73ff846aef457bbdcfe3bf29ebdfd9c
KVNbog <kevin.boghossian@gmail.com>
2018-12-27 20:23:11 +0200

    First Commit

---
+++ Assets/Scripts/OwlSentinelScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OwlSentinelScript : MonoBehaviour {
+	[SerializeField] float owlLifeTime;
+	TrailRenderer trailRenderer;
+	NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Start () {
+		trailRenderer = GetComponent<TrailRenderer>();
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		navMeshAgent.SetDestination(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>().spawnPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+    
+}

b0b761f21846be865fe5f134c009b1f956210435
robertkhayat <robert.g.khayat@gmail.com>
2018-12-27 18:20:33 +0200

    basic AI and path finding

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] float playerSpeed;
     [SerializeField] float playerRotation;
-
+    // [SerializeField] NavMeshAgent agent;
     public GameObject basicAttackPrefab;
+    bool castingOwl;
 
+    [SerializeField] GameObject owlSentinelPrefab;
+    GameObject owlSentinel;
+    NavMeshPath navMeshPath;
+    [SerializeField] float owlLifeTime;
+    float owlLife;
+
+    Vector3 spawnPoint;
     bool isFiring;
 
     Animator mageAnimator;
@@ -20,6 +29,8 @@ public class PlayerScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        navMeshPath = new NavMeshPath();
+        spawnPoint = transform.position;
         fireTime = Time.timeSinceLevelLoad;
         isFiring = false;
         mageAnimator = GetComponent<Animator>();
@@ -28,6 +39,23 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log("haha on you !");
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!GameObject.FindGameObjectWithTag("OwlTag"))
+            {
+                owlSentinel = GameObject.Instantiate(owlSentinelPrefab, transform.position, Quaternion.identity);
+                NavMeshAgent owlAgent = owlSentinel.GetComponent<NavMeshAgent>();
+
+                owlAgent.SetDestination(spawnPoint);
+            }
+            else
+            {
+                Destroy(GameObject.FindGameObjectWithTag("OwlTag"));
+            }
+        }
+
+
         if (Time.timeSinceLevelLoad - fireTime > 1.2f && isFiring)
         {
             isFiring = false;
@@ -45,13 +73,11 @@ public class PlayerScript : MonoBehaviour
             playerSpeed = 0f;
         else
             playerSpeed = 0.22f;
-
-
+            
         PlayerMove();
 
         Input.ResetInputAxes();
     }
-
     void PlayerMove()
     {
         float movementX = Input.GetAxis("Horizontal");
@@ -112,6 +138,16 @@ public class PlayerScript : MonoBehaviour
         /////////////////////////////////////////////////////////////////////
     }
 
+    private void OnBecameInvisible()
+    {
+
+    }
+
+    private void OnBecameVisible()
+    {
+
+    }
+
     void ContinueMoving()
     {
         playerSpeed = 0.22f;
@@ -131,6 +167,12 @@ public class PlayerScript : MonoBehaviour
     {
         Vector3 ballPosition = transform.position + transform.forward * 5f;
         GameObject basicAttack = GameObject.Instantiate(basicAttackPrefab, ballPosition + new Vector3(0, 5f, 0), transform.rotation);
+
+    }
+
+    private void KillOwl()
+    {
+        Destroy(owlSentinel);
     }
 
     void FootR()

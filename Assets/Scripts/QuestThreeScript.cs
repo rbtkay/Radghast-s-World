f735836c2023752e8b7cfd15deee329ea302cbc3
robertkhayat <robert.g.khayat@gmail.com>
2019-01-12 07:12:54 +0200

    feat: questThree almost Done, remaining: fixCollision with trees and spawning pigs from towers

---
+++ Assets/Scripts/QuestThreeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class QuestThreeScript : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent npcAgent;
+    Animator npcAnimator;
+    Vector3 escapeDestination;
+	bool isTouched;
+
+    int escapeCount;
+    string[] destinationString = { "EscapeOneTag", "EscapeTwoTag", "EscapeThreeTag", "EscapeFourTag" };
+
+    // Use this for initialization
+    void Start()
+    {
+        npcAnimator = GetComponent<Animator>();
+        npcAgent = GetComponent<NavMeshAgent>();
+		isTouched = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (npcAgent.speed == 0 && Vector3.Distance(player.transform.position, transform.position) < 10)
+        {
+            Escape();
+        }
+
+        if (Vector3.Distance(escapeDestination, transform.position) < 10)
+        {
+            StopEscape();
+        }
+
+
+    }
+
+    void Escape()
+    {
+        escapeCount = Random.Range(0, 3);
+        escapeDestination = GameObject.FindGameObjectWithTag(destinationString[escapeCount]).transform.position;
+
+        npcAgent.SetDestination(escapeDestination);
+        npcAnimator.SetBool("SprintJump", true);
+        npcAgent.speed = 0.5f;
+    }
+
+    void StopEscape()
+    {
+        npcAnimator.SetBool("SprintJump", false);
+        npcAgent.speed = 0;
+    }
+}
---
+++ Assets/Scripts/TowerBulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBulletScript : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed;
+    Vector3 destination;
+    // Use this for initialization
+    void Start()
+    {
+        destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, bulletSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if (other.gameObject.tag == "QuestThreeTag")
+        // {
+
+        // }
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "MainWorldTag")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
